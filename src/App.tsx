31b8ef60060210a1f63153b507649366e08e6f00
Anas Yaseen <anasyaseen1@outlook.com>
2022-07-22 14:24:24 +0300

    Minor styling adjustments Created about page and added it to paths

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement, useState } from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App(): ReactElement {
   const [theme, setTheme] = useState<string>(
@@ -20,6 +21,7 @@ function App(): ReactElement {
           <Navbar theme={theme} updateTheme={updateTheme} />
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </div>
       </div>
